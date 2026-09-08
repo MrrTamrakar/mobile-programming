a425c37b51cca5475cd760b4989bdbb72605841b
MrrTamrakar <info.mrrtamrakar@gmail.com>
2026-09-08 08:16:38 +0545

    Update layout and added add/ multiply/subtract function.

--- Week2/function.js
+++ Week2/function.js
@@ -5,3 +5,19 @@ function calculateSum() {
     let sum = Number(num1) + Number(num2);
     document.getElementById("result").innerText = "Result: " + sum;
 }
+
+function calculateMul() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let mul = Number(num1) * Number(num2);
+    document.getElementById("result").innerText = "Result: " + mul;
+}
+
+function calculateSub() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let sub = Number(num1) - Number(num2);
+    document.getElementById("result").innerText = "Result: " + sub;
+}
