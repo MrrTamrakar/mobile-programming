b556c6fe32ac8c373915ee1c0145b142d590613e
MrrTamrakar <info.mrrtamrakar@gmail.com>
2026-09-08 07:49:05 +0545

    Add sum calculation code for week 2

---
+++ Week2/function.js
@@ -0,0 +1,7 @@
+function calculateSum() {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+
+    let sum = Number(num1) + Number(num2);
+    document.getElementById("result").innerText = "Result: " + sum;
+}
